d361cdab6611bc8257b48761a7df1c20a2d9ae9e
oqrusk <oqrusk@gmail.com>
2016-12-18 22:27:50 +0900

    enabled remote debug

--- oneline_bbs/bbs.php
+++ oneline_bbs/bbs.php
@@ -17,9 +17,9 @@ $errors = array();
 if( $_SERVER['REQUEST_METHOD'] === 'POST') {
     // 名前の☑
     $name = null;
-    if ( !isset($_POST['name']) || !strlen($_POST['name'])){
+    if (!isset($_POST['name']) || !strlen($_POST['name'])) {
         $errors['name'] = '名前を入力してください';
-    } else if(strlen($_POST['name']) > 40) {
+    } else if (strlen($_POST['name']) > 40) {
         $errors['name'] = '名前は40文字以内で入力してください';
     } else {
         $name = $_POST['name'];
@@ -28,16 +28,16 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST') {
     //　ひとことの☑
     // 名前の☑
     $comment = null;
-    if ( !isset($_POST['comment']) || !strlen($_POST['comment'])){
+    if (!isset($_POST['comment']) || !strlen($_POST['comment'])) {
         $errors['comment'] = 'ひとことを入力してください';
-    } else if(strlen($_POST['name']) > 200) {
+    } else if (strlen($_POST['name']) > 200) {
         $errors['comment'] = 'ひとことは200文字以内で入力してください';
     } else {
         $comment = $_POST['comment'];
     }
 
     // エラーがなければ保存
-    if(count($errors) === 0){
+    if (count($errors) === 0) {
         //保存SQL作成
         $sql = "INSERT INTO `post` (`name`, `comment`, `created_at`) VALUES ('"
             . mysql_real_escape_string($name) . "', '"
@@ -45,9 +45,12 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST') {
             . date('Y-m-d H:i:s') . "')";
         //保存
         mysql_query($sql, $link);
+
+        mysql_close($link);
+
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -67,6 +70,7 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST') {
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
+
         名前: <input type="text" name="name" />
         <br/>
         ひとこと: <input type="text" name="comment" size="60" />
@@ -79,17 +83,19 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
     $result = mysql_query($sql, $link);
     ?>
+
     <?php if($result !== false && mysql_num_rows($result)): ?>
     <ul>
         <?php   while( $post = mysql_fetch_assoc($result)): ?>
         <li>
-            <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>
+            <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
             <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
             - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
         </li>
         <?php endwhile; ?>
     </ul>
     <?php endif; ?>
+
     <?php
         // 取得結果を開放して接続を閉じる
         mysql_free_result($result);
