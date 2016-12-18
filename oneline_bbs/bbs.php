f062ad93810d9ac82538ed32aa5b33684f431c33
oqrusk <oqrusk@gmail.com>
2016-12-18 21:40:43 +0900

    implement post

--- oneline_bbs/bbs.php
+++ oneline_bbs/bbs.php
@@ -1,3 +1,55 @@
+<?php
+
+// DB接続
+
+$link = mysql_connect('0.0.0.0', 'root', 'mysql');
+if(!link) {
+    die('データベースに接続できません：'.mysql_error());
+}
+
+// データベースを選択する
+mysql_select_db('oneline_bbs', $link);
+
+$errors = array();
+
+//POSTなら保存処理
+
+if( $_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 名前の☑
+    $name = null;
+    if ( !isset($_POST['name']) || !strlen($_POST['name'])){
+        $errors['name'] = '名前を入力してください';
+    } else if(strlen($_POST['name']) > 40) {
+        $errors['name'] = '名前は40文字以内で入力してください';
+    } else {
+        $name = $_POST['name'];
+    }
+
+    //　ひとことの☑
+    // 名前の☑
+    $comment = null;
+    if ( !isset($_POST['comment']) || !strlen($_POST['comment'])){
+        $errors['comment'] = 'ひとことを入力してください';
+    } else if(strlen($_POST['name']) > 200) {
+        $errors['comment'] = 'ひとことは200文字以内で入力してください';
+    } else {
+        $comment = $_POST['comment'];
+    }
+
+    // エラーがなければ保存
+    if(count($errors) === 0){
+        //保存SQL作成
+        $sql = "INSERT INTO `post` (`name`, `comment`, `created_at`) VALUES ('"
+            . mysql_real_escape_string($name) . "', '"
+            . mysql_real_escape_string($comment) . "', '"
+            . date('Y-m-d H:i:s') . "')";
+        //保存
+        mysql_query($sql, $link);
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,13 +57,21 @@
 </head>
 <body>
     <h1>ひとこと掲示板</h1>
-
-    <from action="bbs.php" method="post">
+    <form action="bbs.php" method="post">
+        <?php if (count($errors)): ?>
+            <ul>
+                <?php foreach ($errors as $error): ?>
+                <li>
+                    <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
         名前: <input type="text" name="name" />
         <br/>
         ひとこと: <input type="text" name="comment" size="60" />
         <br />
-        <input type="submit" value="送信" />
-    </from>
+        <input type="submit" name="submit" value="送信" />
+    </form>
 </body>
 </html>
