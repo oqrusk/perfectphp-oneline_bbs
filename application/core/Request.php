45ab859f066bcc77a497e74a5ba9575a472a8e85
oqrusk <oqrusk@gmail.com>
2016-12-23 22:59:17 +0900

    implement Response class (:p222)

--- application/core/Request.php
+++ application/core/Request.php
@@ -76,7 +76,7 @@ class Request
             $request_uri = substr($request_uri, 0, $pos);
         }
 
-        $path_info = (string) subst($request_uri, strlen($base_url));
+        $path_info = (string) substr($request_uri, strlen($base_url));
 
         return $path_info;
     }
